52f287c5c650416cb011a5b585cc70d74aa426db
linqing <linqing_07@126.com>
2017-12-20 20:37:02 +0800

    manager

--- e3-manager/e3mall-interface/src/main/java/cn/e3mall/service/TbItemService.java
+++ e3-manager/e3mall-interface/src/main/java/cn/e3mall/service/TbItemService.java
@@ -1,9 +1,13 @@
 package cn.e3mall.service;
 
+import cn.e3mall.pojo.DatagridResult;
 import cn.e3mall.pojo.TbItem;
 
 public interface TbItemService {
 
 	TbItem findById(Long id);
+	
+	DatagridResult findAll(int page,int rows);
+	
 	
 }
--- e3-manager/e3mall-service/src/main/java/cn/e3mall/service/impl/TbItemServiceImpl.java
+++ e3-manager/e3mall-service/src/main/java/cn/e3mall/service/impl/TbItemServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.e3mall.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
 import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.DatagridResult;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.TbItemService;
 @Service
 public class TbItemServiceImpl implements TbItemService {
@@ -14,6 +21,20 @@ public class TbItemServiceImpl implements TbItemService {
 	
 	public TbItem findById(Long id) {
 		return itemMapper.selectByPrimaryKey(id);
+	}
+
+	public DatagridResult findAll(int page, int rows) {
+		PageHelper.startPage(page, rows);
+		
+		TbItemExample example = new TbItemExample();
+		List<TbItem> list = itemMapper.selectByExample(example);
+		
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		long total = pageInfo.getTotal();
+		DatagridResult result = new DatagridResult();
+		result.setTotal((int)total);
+		result.setRows(list);
+		return result;
 	}
 
 }
